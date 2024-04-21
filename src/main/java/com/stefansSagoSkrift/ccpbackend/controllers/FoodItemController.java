59b4de4cbc273a96b5f7119c1206c502fd12371c
Stefan <stefan.frank.sund@gmail.com>
2024-04-21 15:38:37 +0200

    La till CrossOrigin så jag kan köra requests från och till min egna maskin. Samt en enpoint för att uppdatera food item

--- src/main/java/com/stefansSagoSkrift/ccpbackend/controllers/FoodItemController.java
+++ src/main/java/com/stefansSagoSkrift/ccpbackend/controllers/FoodItemController.java
@@ -6,10 +6,13 @@ import com.stefansSagoSkrift.ccpbackend.service.FoodItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:5173")
 public class FoodItemController {
 
     @Autowired
@@ -21,4 +24,9 @@ public class FoodItemController {
         return ResponseEntity.ok().body(createdFoodItem);
     }
 
+    @PostMapping("/updateFoodItem/{mealId}")
+    public ResponseEntity<FoodItemDTO> updateFoodItem(@RequestBody FoodItem foodItem, @PathVariable Long mealId){
+        FoodItemDTO updateFoodItem = foodItemService.updateFoodItem(foodItem, mealId);
+        return ResponseEntity.ok().body(updateFoodItem);
+    }
 }
