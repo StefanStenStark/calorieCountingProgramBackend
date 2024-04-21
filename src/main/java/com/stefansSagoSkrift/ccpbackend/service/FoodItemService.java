766347f8b346b62516de630ab1d3a4871752b5e0
Stefan <stefan.frank.sund@gmail.com>
2024-04-21 15:39:10 +0200

    Delen i servuce för att uppdatera food item

--- src/main/java/com/stefansSagoSkrift/ccpbackend/service/FoodItemService.java
+++ src/main/java/com/stefansSagoSkrift/ccpbackend/service/FoodItemService.java
@@ -2,7 +2,9 @@ package com.stefansSagoSkrift.ccpbackend.service;
 
 import com.stefansSagoSkrift.ccpbackend.DTOs.FoodItemDTO;
 import com.stefansSagoSkrift.ccpbackend.entities.FoodItem;
+import com.stefansSagoSkrift.ccpbackend.entities.Meal;
 import com.stefansSagoSkrift.ccpbackend.repositories.FoodItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +18,35 @@ public class FoodItemService {
         FoodItem newFoodItem = new FoodItem();
         newFoodItem.setName(foodItem.getName());
         newFoodItem.setCalories(foodItem.getCalories());
+        newFoodItem.setNutritionRating(foodItem.getNutritionRating());
 
         foodItemRepository.save(newFoodItem);
 
         FoodItemDTO newFoodItemDTO = new FoodItemDTO();
         newFoodItemDTO.setCalories(foodItem.getCalories());
         newFoodItemDTO.setName(foodItem.getName());
+        newFoodItemDTO.setNutritionRating(foodItem.getNutritionRating());
 
         return newFoodItemDTO;
+    }
+
+    public FoodItemDTO updateFoodItem(FoodItem foodItem, Long mealId) {
+        FoodItem updatedFoodItem = foodItemRepository.findById(mealId)
+                .orElseThrow(() -> new EntityNotFoundException("Did not find item with id: " + mealId));
+
+        updatedFoodItem.setName(foodItem.getName());
+        updatedFoodItem.setCalories(foodItem.getCalories());
+        updatedFoodItem.setNutritionRating(foodItem.getNutritionRating());
+
+        foodItemRepository.save(updatedFoodItem);
+
+        FoodItemDTO updatedFoodItemInfo = new FoodItemDTO();
+
+        updatedFoodItemInfo.setName(updatedFoodItem.getName());
+        updatedFoodItemInfo.setCalories(updatedFoodItem.getCalories());
+        updatedFoodItemInfo.setNutritionRating(updatedFoodItem.getNutritionRating());
+        return updatedFoodItemInfo;
+
+
     }
 }
